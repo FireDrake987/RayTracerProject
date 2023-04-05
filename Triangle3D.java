2ae6020ac321b1ad54192337fb6ab2333c02fc7a
FireDrake987 <129759669+FireDrake987@users.noreply.github.com>
2023-04-05 09:16:25 -0500

    Delete Triangle3D.java
    Its not needed and not going to be implemented.

--- Triangle3D.java
+++
@@ -1,8 +0,0 @@
-package rayTracing;
-
-public class Triangle3D {
-	public Point3D[] points;
-	public Triangle3D(Point3D pointA, Point3D pointB, Point3D pointC) {
-		points = new Point3D[] {pointA, pointB, pointC};
-	}
-}
